375c982915ffcd6aa7144f648d6db5f3bb82e6c5
PedroVitor8 <pedrovitoliveradasilva@gmail.com>
2025-06-12 19:35:22 -0300

    Views Criada

--- projeto/todos/views.py
+++ projeto/todos/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
+from .models import Todo
+from django.urls import reverse_lazy
 # Create your views here.
+
+
+class TodoListView(ListView):
+    model = Todo
+    
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ['title', 'deadline']
+    success_url = reverse_lazy('todo_list')
+    
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ['title', 'deadline']
+    success_url = reverse_lazy('todo_list')
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = reverse_lazy('todo_list')
