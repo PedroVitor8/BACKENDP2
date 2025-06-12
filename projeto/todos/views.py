fbdee6a8b6c0e0882314ea6d12f044256c2b33ac
PedroVitor8 <pedrovitoliveradasilva@gmail.com>
2025-06-12 20:07:31 -0300

    complete adicionado ao sistema

--- projeto/todos/views.py
+++ projeto/todos/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from .models import Todo
 from django.urls import reverse_lazy
+from datetime import date
 # Create your views here.
 
 
@@ -21,3 +22,17 @@ class TodoUpdateView(UpdateView):
 class TodoDeleteView(DeleteView):
     model = Todo
     success_url = reverse_lazy('todo_list')
+    
+class TodoCompleteView(View):
+    def get(self, request, pk):
+        Todo.objects.get(pk=pk)
+        Todo.finished_at = date.today()
+        Todo.save()
+        return redirect("todo_list")
+        
+class TodoCompleteView(View):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        todo.finished_at = date.today()
+        todo.save()
+        return redirect("todo_list")
